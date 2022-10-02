557d3dba55711632dcff16b65e4e2c7b2ce3948e
SHUBHAM TRIPATHI <52843961+ShubhamTrip@users.noreply.github.com>
2022-10-02 22:22:25 +0530

    Word Counter
    Here We have learnt the use of  Filter function, Split Function, Trim Function

---
+++ JavaScript/Word Counter/script.js
@@ -0,0 +1,25 @@
+var textar = document.getElementById('textArea');
+var wordcount = document.getElementById('wordsp');
+var charcount = document.getElementById('charsp');
+
+textar.addEventListener('input',calculations);
+
+function calculations(){
+
+  var text = textar.value;
+  let char = text.length;
+
+  charcount.innerHTML=char;
+
+  text = text.trim();    // trims all spaces before and after string //  Not nessarcary to use here..
+
+  let words = text.split(" ");  // splits when get space and stores as array of words in words..
+
+
+  let cleanList = words.filter(function(elm){    //  filters and stores
+     return elm !="";
+  });
+
+  wordcount.innerHTML=cleanList.length;
+
+}
